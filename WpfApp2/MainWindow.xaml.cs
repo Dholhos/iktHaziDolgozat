e503d733939221f89a2be55a3aa4fd23c3aa8df7
FeketeClaw <FeketeClaw@REVISION-PC>
2023-05-19 13:46:08 +0000

    Add project files.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Data.SqlTypes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Szeleromu> eromu = new List<Szeleromu>(); 
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            StreamReader sr = new StreamReader("szeleromu.txt");
+            string sor = sr.ReadLine();
+            while (sor != null)
+            {
+                eromu.Add(sor);
+            }
+            sr.Close();
+        }
+
+        private void btnOsszes_Click(object sender, RoutedEventArgs e)
+        {
+            var osszes = eromu.Count(x => x)
+            lbnOsszes.Content = osszes;
+        }
+
+        private void btnAtlag_Click(object sender, RoutedEventArgs e)
+        {
+            var atlag = eromu.Where(x => x.)
+        }
+    }
+}
---
+++ WpfApp2/Szeleromu.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    class Szeleromu
+    {
+        string helyszin;
+        int egyseg;
+        int teljesitmeny;
+        int mukodesEve;
+
+        public Szeleromu(string helyszin, int egyseg, int teljesitmeny, int mukodesEve)
+        {
+            this.helyszin = helyszin;
+            this.egyseg = egyseg;
+            this.teljesitmeny = teljesitmeny;
+            this.mukodesEve = mukodesEve;
+        }
+
+        public string Helyszin { get => helyszin;}
+        public int Egyseg { get => egyseg;}
+        public int Teljesitmeny { get => teljesitmeny;}
+        public int MukodesEve { get => mukodesEve;}
+
+        public char kategoria()
+        {
+            if (teljesitmeny > 1000)
+            {
+                return('A');
+            }
+            if (teljesitmeny < 1000 && teljesitmeny > 500)
+            {
+                return('B');
+            }
+            else
+            {
+                return ('C');
+            }
+        }
+    }
+}
